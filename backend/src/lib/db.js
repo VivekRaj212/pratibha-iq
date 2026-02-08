e3d5bab9fc491b541a5fbecdaf40e685876b2785
VivekRaj212 <rajbharvivek513@gmail.com>
2026-02-08 22:29:33 +0530

    auth and db sync

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config({quiet: true});
+dotenv.config({ quiet: true });
 
 export const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(
       `☑️MongoDB connection successfully established ${conn.connection.host}`,
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import Path from "path";
+import { serve } from "inngest/express";
+import { inngest } from "./lib/inngest.js";
+import { functions } from "./lib/inngest.js";
 dotenv.config({ quiet: true });
 
 const app = express();
+
+const __dirname = Path.resolve();
+
+// middleware
+app.use(express.json());
+// CREDITIALS: TRUE => SERVER ALLOWS A BROWSER TO INCLUDE COOKIES ON REQUEST
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({client: inngest,functions}));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
