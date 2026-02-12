6a8bf164b694fb60e60c24cb41d05b3621fbe080
VivekRaj212 <rajbharvivek513@gmail.com>
2026-02-12 15:32:37 +0530

    auth,inggest and stream sdk sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pratibha-iq" });
@@ -9,25 +10,30 @@ const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
   async ({ event }) => {
+    console.log("🔥 SYNC USER FUNCTION EXECUTED");
+    console.log("EVENT ID:", event.data.id);
     await connectDB();
     const { id, email_addresses, first_name, last_name, image_url } =
       event.data;
-    
+
     const primaryEmail = email_addresses?.[0]?.email_address;
     if (!primaryEmail) {
       throw new Error("User has no email address");
     }
-    
+
     const newUser = {
       clerkId: id,
       email: primaryEmail,
       name: `${first_name || ""} ${last_name || ""}`,
-      profileImg: image_url,
-    };
-      name: `${first_name || ""} ${last_name || ""}`,
-      profileImage: profile_image_url,
+      profileImage: image_url,
     };
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId,
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   },
 );
 
@@ -38,6 +44,8 @@ const deleteUserFromDB = inngest.createFunction(
     await connectDB();
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
   },
 );
 
