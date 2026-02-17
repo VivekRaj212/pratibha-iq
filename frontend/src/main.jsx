2c6e341f12c02a5527b9dbbabb61b1cbc92b2591
VivekRaj212 <rajbharvivek513@gmail.com>
2026-02-17 11:31:57 +0530

    fixeded queryClient variable typo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,12 +12,12 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
-const queyClient = new QueryClient();
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queyClient}>
+      <QueryClientProvider client={queryClient}>
         <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
           <App />
         </ClerkProvider>
